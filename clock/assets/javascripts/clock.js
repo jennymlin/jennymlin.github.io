2f28fc3f563d44bfbe1c39669044566ae1226981
Jenny Lin <jenny@upstart.com>
2016-01-26 15:08:11 -0800

    Make background color update with time

--- clock/assets/javascripts/clock.js
+++ clock/assets/javascripts/clock.js
@@ -1,11 +1,17 @@
 function updateClock() {
-  var clock = document.getElementById("clock-text");
+  var clock = document.getElementById("clock");
+  var background = document.getElementById("background");
   var date = new Date();
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
-  var time = hours + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
-  clock.innerHTML = time;
+  var time = [hours, ("0" + minutes).slice(-2), ("0" + seconds).slice(-2)];
+
+  var display = time.join(":");
+  var color = "#" + time.join("");
+
+  clock.innerHTML = display;
+  background.style.backgroundColor = color;
 
   setTimeout(updateClock, 1000);
 }
