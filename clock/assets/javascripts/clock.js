52f629941df5ab9ab8e49f67e6a898bfc286d1fd
Jenny Lin <jenny@upstart.com>
2016-01-26 14:42:18 -0800

    Make the clock update every second

--- clock/assets/javascripts/clock.js
+++ clock/assets/javascripts/clock.js
@@ -1,8 +1,15 @@
-$(document).ready(function() {
+function updateClock() {
   var clock = document.getElementById("clock-text");
   var date = new Date();
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
-  clock.innerHTML = hours + ":" + minutes + ":" + seconds;
+  var time = hours + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+  clock.innerHTML = time;
+
+  setTimeout(updateClock, 1000);
+}
+
+$(document).ready(function() {
+  updateClock();
 });
