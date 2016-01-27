565b68da3b79b38d228427c29fe92617b7b32138
Jenny Lin <jenny@upstart.com>
2016-01-27 00:54:15 -0800

    Fix hours to show leading 0

--- clock/assets/javascripts/clock.js
+++ clock/assets/javascripts/clock.js
@@ -5,7 +5,7 @@ function updateClock() {
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
-  var time = [hours, ("0" + minutes).slice(-2), ("0" + seconds).slice(-2)];
+  var time = [("0" + hours).slice(-2), ("0" + minutes).slice(-2), ("0" + seconds).slice(-2)];
 
   var color = "#" + time.join("");
 
